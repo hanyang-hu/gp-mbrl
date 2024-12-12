31a08248a9c83bb814d06d6bb7937175eb50c672
Hanyang Hu <63213964+KoHomerHu@users.noreply.github.com>
2024-12-12 14:40:03 +0800

    Change training metric logger

--- toy_examples/train.py
+++ toy_examples/train.py
@@ -10,6 +10,15 @@ from agent import TDMPC
 from utils import Episode, ReplayBuffer
 
 
+def update_metric(metrics, new_metrics):
+    """Update a dictionary of metrics with new metrics."""
+    for k, v in new_metrics.items():
+        if k in metrics:
+            metrics[k].append(v)
+        else:
+            metrics[k] = [v,]
+
+
 def evaluate(env, agent, num_episodes, step, episode_length, action_repeat, render):
     """Evaluate a trained agent and optionally save a video."""
     episode_rewards = []
@@ -33,6 +42,11 @@ def evaluate(env, agent, num_episodes, step, episode_length, action_repeat, rend
 
 def train(cfg_path = "./default.yaml"):
     cfg = OmegaConf.load(cfg_path)
+    
+    # Set random seeds
+    torch.manual_seed(cfg.seed)
+    np.random.seed(cfg.seed)
+
     env = gym.make(cfg.task, render_mode="rgb_array")
     cfg.obs_dim = env.observation_space.shape[0]
     cfg.action_dim = env.action_space.shape[0]
@@ -42,10 +56,11 @@ def train(cfg_path = "./default.yaml"):
     buffer = ReplayBuffer(cfg)
 
     # Run training (adapted from https://github.com/nicklashansen/tdmpc/)
+    train_metrics = {}
     episode_idx, start_time = 0, time.time()
     for step in range(0, cfg.train_steps+cfg.episode_length, cfg.episode_length):
         # Collect trajectory
-        obs, _ = env.reset()
+        obs, _ = env.reset(seed=cfg.seed)
         agent.reset_correction() # do nothing for TD-MPC
         episode = Episode(cfg, obs)
         while not episode.done:
@@ -55,18 +70,15 @@ def train(cfg_path = "./default.yaml"):
             agent.correction(obs, action, next_obs, reward, done) # do nothing for TD-MPC
             obs = next_obs
             episode += (obs, action, reward, done)
-        # print(len(episode), cfg.episode_length)
-        # assert len(episode) == cfg.episode_length
+        assert len(episode) == cfg.episode_length
         buffer += episode
 
         # Update model
-        train_metrics = {}
         if step >= cfg.seed_steps:
             num_updates = cfg.seed_steps if step == cfg.seed_steps else cfg.episode_length
             progress_bar = tqdm.tqdm(range(num_updates), desc=f"Episode {episode_idx}")
             for _ in progress_bar:
                 loss = agent.update(buffer, step)
-                train_metrics.update(loss)
                 progress_bar.set_postfix({"Weighted Loss": loss["weighted_loss"]})
 
         # Log training episode
@@ -77,11 +89,12 @@ def train(cfg_path = "./default.yaml"):
             'step': step,
             'env_step': env_step,
             'total_time': time.time() - start_time,
-            'episode_reward': episode.cumulative_reward}
-        train_metrics.update(common_metrics)
+            'episode_reward': episode.cumulative_reward
+        }
+        update_metric(train_metrics, common_metrics)
 
         # Evaluate and visualize agent periodically
-        if env_step != 0 and env_step % int(eval(cfg.eval_freq)) == 0:
+        if cfg.eval and env_step != 0 and env_step % cfg.eval_freq == 0:
             with torch.no_grad():
                 eval_env = gym.make(cfg.task, render_mode="human")
                 evaluate(eval_env, agent, cfg.eval_episodes, step, int(eval(cfg.val_episode_length)), action_repeat=cfg.action_repeat, render=True)
@@ -91,6 +104,7 @@ def train(cfg_path = "./default.yaml"):
 
     # Save model
     if cfg.save_path:
+        os.makedirs(os.path.dirname(cfg.save_path), exist_ok=True)
         agent.save(cfg.save_path)
         print(f'Model saved to {cfg.save_path}')
     # Save training metrics to "./results/{cfg.task}/metrics.csv"
@@ -98,9 +112,9 @@ def train(cfg_path = "./default.yaml"):
     os.makedirs(f"./results/{cfg.task}", exist_ok=True)
     with open(f"./results/{cfg.task}/metrics.csv", "w") as f:
         f.write("episode,step,env_step,total_time,episode_reward\n")
-        for k, v in train_metrics.items():
-            f.write(f"{k},{v}\n")
-
+        for i in range(episode_idx):
+            f.write(f"{train_metrics['episode'][i]},{train_metrics['step'][i]},{train_metrics['env_step'][i]},{train_metrics['total_time'][i]},{train_metrics['episode_reward'][i]}\n")
+    print(f"Training metrics saved to ./results/{cfg.task}/metrics.csv")
 
 if __name__ == "__main__":
     train()
